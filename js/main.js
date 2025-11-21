62f54198f83c0d68d3b39432ce7c2b859968505d
FranciscoPerezPerez <l21200126@pachuca.tecnm.mx>
2025-11-21 10:38:24 -0600

    Actualizar URL localtunnel 3

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ class CarMonitor {
 
     connectWebSocket() {
         // ✅ URL ACTUALIZADA CON SERVEO
-        const serverUrl = 'wss://petite-jokes-find.loca.lt';
+        const serverUrl = 'wss://silent-boats-sin.loca.lt';
         
         console.log('Conectando monitor a:', serverUrl);
         this.ws = new WebSocket(serverUrl);
